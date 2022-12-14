27f093ec8717543e89279552e5d6bdbf8b55d474
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2022-12-14 16:58:29 +0530

    20221214

--- java/no-package/classobject/Complex.java
+++ java/no-package/classobject/Complex.java
@@ -13,15 +13,25 @@ class Complex {
 
     Complex plus(Complex c) {
         Complex t = new Complex();
-
+        t.real = this.real + c.real;
+        t.imaginary = this.imaginary + c.imaginary;
         return t;
     }
 
+    float getReal() {
+        return this.real;
+    }
+
+    float getImaginary() {
+        return this.imaginary;
+    }
+
     public static void main(String[] args) {
-        Complex c1 = new Complex();
-        Complex c2 = new Complex();
+        Complex c1 = new Complex(11.5f, 10.6f);
+        Complex c2 = new Complex(15.7f, 20.9f);
         Complex c3;
         c3 = c1.plus(c2);
+        System.out.printf("The sum is %1.2f + %1.2fi\n", c3.getReal(), c3.getImaginary());
     }
 }
 
